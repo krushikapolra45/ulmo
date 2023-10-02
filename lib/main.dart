94398d5a7ed297ac57a74ddc5c5f579d6cf11e12
krushika Polra <kpolara548@gmail.com>
2023-10-02 11:16:20 +0530

    change

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:ulmo/filter_option_screen.dart';
+import 'package:ulmo/main_page_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       // initialRoute: Routename.initialRoute,
       // routes: AppRoute().approutes,
 
-      home: const FilterOptionScreen(),
+      home: const MainPageScreen(),
     );
   }
 }
